19ee7da1fa1d2aa4d0fb6d616eac8e04b2dbd1f4
GUBBALA VENKATA VARSHINI <gubbalavarshini25@gmail.com>
2025-06-09 15:42:29 +0530

    Add files via upload

---
+++ Inheritance.java
@@ -0,0 +1,25 @@
+class Animal 
+{
+    void eat() 
+    {
+        System.out.println("This animal eats food");
+    }
+}
+
+class Dog extends Animal
+{
+    void bark() 
+    {
+        System.out.println("Dog barks");
+    }
+}
+
+public class Inheritance
+{
+    public static void main(String[] args) 
+    {
+        Dog d = new Dog();
+        d.eat();  
+        d.bark();
+    }
+}
